a360563016aa4383b5951dd2d0e8c7697ebabe8b
Ilya Gellert <gellertilya@gmail.com>
2025-06-09 03:29:23 +0300

    Update pool.go, add methods: Addworker, addNewWorker, RemoveWorker, popWorker, Start, stopAllWorkers, Shutdown, SubmitJob ./internal/pool/pool.go

--- internal/pool/pool.go
+++ internal/pool/pool.go
@@ -27,9 +27,90 @@ func NewPool(initialWorkers int) *Pool {
 
 	logrus.Infof("Creating a pool with %d initial workers", initialWorkers)
 	for i := 0; i < initialWorkers; i++ {
-		// TODO Create method addNewWorker()
-		//p.addNewWorker()
+		p.addNewWorker()
 	}
 
 	return p
+}
+
+func (p *Pool) AddWorker() {
+	p.addWorker <- struct{}{}
+}
+
+func (p *Pool) addNewWorker() {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	worker := NewWorker(p.nextWorkerID, p.jobs)
+
+	p.workers[worker.id] = worker
+	p.nextWorkerID++
+
+	go worker.Start()
+}
+
+func (p *Pool) RemoveWorker() {
+	p.removeWorker <- struct{}{}
+}
+
+func (p *Pool) popWorker() {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	if len(p.workers) == 0 {
+		logrus.Info("No workers left")
+		return
+	}
+
+	maxWorkerID := -1
+	for id := range p.workers {
+		if id > maxWorkerID {
+			maxWorkerID = id
+		}
+	}
+
+	if worker, ok := p.workers[maxWorkerID]; ok {
+		worker.Stop()
+		delete(p.workers, maxWorkerID)
+	}
+}
+
+func (p *Pool) Start() {
+	go func() {
+		logrus.Info("Starting pool")
+		for {
+			select {
+			case <-p.addWorker:
+				p.addNewWorker()
+			case <-p.removeWorker:
+				p.popWorker()
+			case <-p.shutdown:
+				p.stopAllWorkers()
+				logrus.Info("Pool shutting down")
+				return
+			}
+		}
+	}()
+}
+
+func (p *Pool) stopAllWorkers() {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	logrus.Info("Stopping all workers")
+	for id, worker := range p.workers {
+		worker.Stop()
+		delete(p.workers, id)
+	}
+	logrus.Info("All workers are stopped")
+
+	close(p.jobs)
+}
+
+func (p *Pool) Shutdown() {
+	close(p.shutdown)
+}
+
+func (p *Pool) SubmitJob(job string) {
+	p.jobs <- job
 }
