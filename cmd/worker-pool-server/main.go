7550abac96ef3c8f0cbbef5aea1c390019ca4ac1
Ilya Gellert <gellertilya@gmail.com>
2025-06-09 21:59:49 +0300

    Update exit in run.go, Add autodoc

--- cmd/worker-pool-server/main.go
+++ cmd/worker-pool-server/main.go
@@ -2,6 +2,9 @@ package main
 
 import "github.com/l-ILINDAN-l/WorkerPool/cmd"
 
+// main is the entry point for the application.
+// Its sole responsibility is to execute the root command from the cmd package,
+// which in turn handles all command-line logic.
 func main() {
 	cmd.Execute()
 }
